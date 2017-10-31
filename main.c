553409f4e3228604c410cfbb9aa2dc64823d7931
luchk <30504944+luchk@users.noreply.github.com>
2017-10-31 23:21:52 +0200

    Пята лаба до Злобіна (здана)

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x){
+    return exp(x) - exp(-x) - 2;
+}
+double fp(double x){
+    return exp(x)-exp(-x);
+}
+int main() {
+
+    double x=1,dx, eps, test;
+    eps = 1e-16;
+    do {
+        dx = f(x)/fp(x);
+        x = x-dx;
+         //printf("x = %1.20Lf f(x) = %1.20Lf \n", x , f(x));
+    }while(fabs(dx)>eps);
+
+    printf("x = %.10Lf f(x) = %.10f \n", x , f(x));
+    return 0;
+}
